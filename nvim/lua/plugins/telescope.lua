b1385e2b6225a9bd408708e12a309f20ecc695bd
Alper Doğan <alperuni217@gmail.com>
2023-05-31 11:38:31 +0300

    feat: change

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,12 +2,17 @@ return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.1",
 	config = function()
+		require('helpers')
 		local commonDropdown = {
 			previewer = false,
 			theme = "dropdown",
 		}
 
 		local telescope = require("telescope")
+
+		local filePicker = ConcatTables(commonDropdown, {
+			hidden = true,
+		})
 
 		telescope.setup({
 			defaults = {
@@ -18,12 +23,12 @@ return {
 						preview_width = 0.6,
 					},
 				},
-				prompt_prefix = ">  ",
+				prompt_prefix = " > ",
 			},
 			pickers = {
 				colorscheme = commonDropdown,
-				oldfiles = commonDropdown,
-				find_files = commonDropdown,
+				oldfiles = filePicker,
+				find_files = filePicker,
 				buffers = commonDropdown,
 			},
 			extensions = {
@@ -42,7 +47,8 @@ return {
 		vim.keymap.set("n", "<leader>f", "<cmd>Telescope live_grep<cr>", { desc = "Space [F]ind" })
 		vim.keymap.set("n", "<leader>wf", "<cmd>Telescope grep_string<cr>", { desc = "[W]ord [F]ind" })
 		vim.keymap.set("n", "<leader>sd", "<cmd>Telescope diagnostics<cr>", { desc = "[S]how [D]iagnostics" })
-		vim.keymap.set("n", "<leader><leader>", "<cmd>Telescope<cr>", { desc = "Map space-space to see Telescope menu" })
+		vim.keymap.set("n", "<leader><leader>", "<cmd>Telescope<cr>",
+			{ desc = "Map space-space to see Telescope menu" })
 		vim.keymap.set("n", "<leader>rf", function()
 			require("telescope.builtin").oldfiles({
 				cwd_only = true,
