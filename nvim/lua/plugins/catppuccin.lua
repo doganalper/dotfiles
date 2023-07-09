1969878443e40495bcdf3e585e4d8d892f47240d
Alper Doğan <alperuni217@gmail.com>
2023-07-09 21:22:39 +0300

    Revert "test: comment catppuccin flavour"
    This reverts commit 0d0ec9372a50524bb81ef95c196d8e99f26a5911.

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -4,7 +4,7 @@ return {
 	priority = 1000,
 	config = function()
 		require('catppuccin').setup({
-			-- flavour = 'frappe',
+			flavour = 'frappe',
 			background = { -- :h background
 				light = "latte",
 				dark = "frappe",
